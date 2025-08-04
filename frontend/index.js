9c76846edfe50978e627263cbc9b549902f9afe7
jihohwang <164844858+jihohwang@users.noreply.github.com>
2025-08-04 20:45:55 +0900

    feat: 글쓰기 기능 구현
    - 프론트엔드 글쓰기 페이지 form으로 작업했습니다.
    - 백엔드와 SQLite를 연동해서 이미지 및 데이터를 업로드하는 기능을 구현했습니다.

---
+++ frontend/index.js
@@ -0,0 +1,62 @@
+const calcTime = (timestamp) => {
+  const now = Date.now();
+  const diffMs = now - timestamp;
+  const diffSec = Math.floor(diffMs / 1000);
+  const diffMin = Math.floor(diffSec / 60);
+  const diffHour = Math.floor(diffMin / 60);
+  const diffDay = Math.floor(diffHour / 24);
+
+  if (diffDay > 0) return `${diffDay}일 전`;
+  else if (diffHour > 0) return `${diffHour}시간 전`;
+  else if (diffMin > 0) return `${diffMin}분 전`;
+  else if (diffSec > 0) return `${diffSec}초 전`;
+  else return "방금 전";
+};
+
+const renderData = (data) => {
+  const main = document.querySelector("main");
+  data.reverse().forEach(async (obj) => {
+    const div = document.createElement("div");
+    div.className = "item-list";
+
+    const imgDiv = document.createElement("div");
+    imgDiv.className = "item-list__img";
+
+    const img = document.createElement("img");
+    const res = await fetch(`/images/${obj.id}`);
+    const blob = await res.blob();
+    const url = URL.createObjectURL(blob);
+    img.src = url;
+
+    const InfoDiv = document.createElement("div");
+    InfoDiv.className = "item-list__info";
+
+    const InfoTitleDiv = document.createElement("div");
+    InfoTitleDiv.className = "item-list__info-title";
+    InfoTitleDiv.innerText = obj.title;
+
+    const InfoMetaDiv = document.createElement("div");
+    InfoMetaDiv.className = "item-list__info-meta";
+    InfoMetaDiv.innerText = obj.place + " " + calcTime(obj.insertAt);
+
+    const InfoPriceDiv = document.createElement("div");
+    InfoPriceDiv.className = "item-list__info-price";
+    InfoPriceDiv.innerText = obj.price;
+
+    imgDiv.appendChild(img);
+    InfoDiv.appendChild(InfoTitleDiv);
+    InfoDiv.appendChild(InfoMetaDiv);
+    InfoDiv.appendChild(InfoPriceDiv);
+    div.appendChild(imgDiv);
+    div.appendChild(InfoDiv);
+    main.appendChild(div);
+  });
+};
+
+const fetchList = async () => {
+  const res = await fetch("/items");
+  const data = await res.json();
+  renderData(data);
+};
+
+fetchList();
